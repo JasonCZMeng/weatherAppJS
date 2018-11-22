27ec145450abab18cb98453656fc9d55a8b30c7b
Jason Meng <JasonCZMeng@gmail.com>
2018-11-22 14:09:59 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '630c24f246f2c0e57a56e1fa0f7e9539';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
